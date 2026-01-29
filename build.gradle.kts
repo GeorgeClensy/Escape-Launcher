37e00a83a3a3e392b5a45a4e59b0ec2fcdc52fca
George Clensy <george.clensy@gmail.com>
2026-01-29 20:47:31 +0000

    Update project dependencies and Gradle properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,6 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
-    id("com.google.devtools.ksp") version "2.2.21-2.0.4" apply false
+    id("com.google.devtools.ksp") version "2.3.2" apply false
     alias(libs.plugins.compose.compiler) apply false
 }
