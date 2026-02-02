be934e48eb3e925be220bc2a9270f3cec1d4f115
fm-sys <64581222+fm-sys@users.noreply.github.com>
2026-02-02 15:37:54 +0100

    fix FOSS build WeatherImpl

--- app/src/foss/java/com/geecee/escapelauncher/utils/WeatherImpl.kt
+++ app/src/foss/java/com/geecee/escapelauncher/utils/WeatherImpl.kt
@@ -3,7 +3,7 @@ package com.geecee.escapelauncher.utils
 import android.content.Context
 
 class WeatherImpl : WeatherProxy {
-    override fun getWeather(context: Context, callback: (String) -> Unit) {
+    override fun getWeather(context: Context, useFarenheit: Boolean, callback: (String) -> Unit) {
         callback("Weather is not available on the FOSS version of the app")
     }
 }
