d34e74bf357ed8c3f685d2d48644500f2bb086af
George Clensy <george.clensy@gmail.com>
2026-03-13 18:06:00 +0000

    Fixed app info page not showing normal work apps one for work apps

--- core/common/src/main/java/com/geecee/escapelauncher/core/common/AppUtils.kt
+++ core/common/src/main/java/com/geecee/escapelauncher/core/common/AppUtils.kt
@@ -1,11 +1,13 @@
 package com.geecee.escapelauncher.core.common
 
+import android.app.ActivityOptions
 import android.content.Context
 import android.content.Intent
-import android.provider.Settings
+import android.content.pm.LauncherApps
+import android.graphics.Rect
 import androidx.core.net.toUri
 
-fun uninstallApp(context: Context, app: InstalledApp){
+fun uninstallApp(context: Context, app: InstalledApp) {
     val intent = Intent(Intent.ACTION_DELETE).apply {
         data = "package:${app.packageName}".toUri()
         putExtra(Intent.EXTRA_USER, app.user)
@@ -14,11 +16,23 @@ fun uninstallApp(context: Context, app: InstalledApp){
     context.startActivity(intent)
 }
 
-fun goToAppInfo(context: Context, app: InstalledApp){
-    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
-        data = "package:${app.packageName}".toUri()
-        putExtra(Intent.EXTRA_USER, app.user)
-        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+fun goToAppInfo(
+    context: Context,
+    app: InstalledApp,
+    sourceBounds: Rect? = null
+) {
+    val launcherApps =
+        context.getSystemService(Context.LAUNCHER_APPS_SERVICE) as? LauncherApps ?: return
+    val profile = app.user
+
+    val options = ActivityOptions.makeBasic()
+    if (sourceBounds != null) {
+        options.launchBounds = sourceBounds
     }
-    context.startActivity(intent)
+    launcherApps.startAppDetailsActivity(
+        app.componentName,
+        profile,
+        sourceBounds,
+        options.toBundle()
+    )
 }
