6fb16095a146e7aa95486be85803d99c2551d572
George Clensy <george.clensy@gmail.com>
2026-07-06 20:36:37 +0100

    Logo on welcome page can bounce now

--- feature/onboarding/src/main/java/com/geecee/escapelauncher/feature/onboarding/welcome/WelcomePage.kt
+++ feature/onboarding/src/main/java/com/geecee/escapelauncher/feature/onboarding/welcome/WelcomePage.kt
@@ -1,11 +1,16 @@
 package com.geecee.escapelauncher.feature.onboarding.welcome
 
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.EaseInOutSine
 import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,8 +20,12 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
@@ -24,10 +33,18 @@ import androidx.compose.ui.unit.dp
 import com.geecee.escapelauncher.core.theme.EscapeThemePreview
 import com.geecee.escapelauncher.core.ui.R
 import com.geecee.escapelauncher.core.ui.composables.BlurryCircle
+import com.geecee.escapelauncher.core.ui.utils.doHapticFeedBack
 import com.geecee.escapelauncher.core.ui.utils.toAndroidColor
+import kotlinx.coroutines.launch
 
 @Composable
 fun WelcomePage() {
+    val coroutineScope = rememberCoroutineScope()
+
+    // Tapping the logo stuff
+    val haptics = LocalHapticFeedback.current
+    val iconScale = remember { Animatable(initialValue = 1f) }
+
     // Stuff to make the cirlces move
     val infiniteTransition = rememberInfiniteTransition(label = "FloatingCircles")
     val leftCirlceYOffset by infiniteTransition.animateFloat(
@@ -76,7 +93,31 @@ fun WelcomePage() {
                 "Escape Launcher Logo",
                 Modifier
                     .padding(3.dp)
-                    .align(Alignment.CenterHorizontally),
+                    .align(Alignment.CenterHorizontally)
+                    .graphicsLayer {
+                        scaleX = iconScale.value
+                        scaleY = iconScale.value
+                    }
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null
+                    ) {
+                        doHapticFeedBack(
+                            hapticFeedback = haptics,
+                            enabled = true
+                        )
+
+                        coroutineScope.launch {
+                            iconScale.animateTo(0.8f, animationSpec = tween(durationMillis = 50))
+                            iconScale.animateTo(
+                                targetValue = 1f,
+                                animationSpec = spring(
+                                    dampingRatio = Spring.DampingRatioHighBouncy,
+                                    stiffness = Spring.StiffnessMedium
+                                )
+                            )
+                        }
+                    },
                 tint = MaterialTheme.colorScheme.primary
             )
         }
