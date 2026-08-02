64e5543978732d8392c967ab0874823598b08fd5
George Clensy <george.clensy@gmail.com>
2026-08-02 20:26:12 +0100

    Updated FavouritesViewModel to use new useCase

--- feature/onboarding/src/main/java/com/geecee/escapelauncher/feature/onboarding/favorites/FavoritesViewModel.kt
+++ feature/onboarding/src/main/java/com/geecee/escapelauncher/feature/onboarding/favorites/FavoritesViewModel.kt
@@ -2,13 +2,13 @@ package com.geecee.escapelauncher.feature.onboarding.favorites
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.geecee.escapelauncher.core.domain.apps.GetFavoriteAppsUseCase
 import com.geecee.escapelauncher.core.domain.repository.AppsRepository
 import com.geecee.escapelauncher.core.domain.repository.ModifiedAppsRepository
 import com.geecee.escapelauncher.core.model.InstalledApp
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -16,22 +16,17 @@ import javax.inject.Inject
 @HiltViewModel
 class FavoritesViewModel @Inject constructor(
     appsRepository: AppsRepository,
-    private val modifiedAppsRepository: ModifiedAppsRepository
+    private val modifiedAppsRepository: ModifiedAppsRepository,
+    getFavoriteAppsUseCase: GetFavoriteAppsUseCase
 ) : ViewModel() {
     val installedApps = appsRepository.mainUserApps
 
-    val favoriteApps: StateFlow<List<InstalledApp>> = combine(
-        appsRepository.mainUserApps,
-        modifiedAppsRepository.getFavouriteAppsInOrderFlow()
-    ) { apps, entities ->
-        entities.mapNotNull { entity ->
-            apps.find { it.packageName == entity.packageId }
-        }
-    }.stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(5000),
-        initialValue = emptyList()
-    )
+    val favoriteApps: StateFlow<List<InstalledApp>> = getFavoriteAppsUseCase()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = emptyList()
+        )
 
     fun addFavorite(packageName: String) {
         viewModelScope.launch {
